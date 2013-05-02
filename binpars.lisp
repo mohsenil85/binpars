55639ab209335560b8724f59cdd16614e20c5360
mohsenil85 <mohsenil85@gmail.com>
2013-05-01 23:05:26 -0600

    all abstract everything

--- binpars.lisp
+++ binpars.lisp
@@ -28,25 +28,25 @@
         do (write-byte (char-code char) out))
   (write-byte (char-code +null+) out))
 
-(defclass id3-tag ()
-  ((identifier     :initarg :identifier     :accessor identifier)
-   (major-version  :initarg :major-version  :accessor major-version)
-   (revision       :initarg :revision       :accessor revision)
-   (flag           :initarg :flag           :accessor flag)
-   (size           :initarg :size           :accessor size)
-   (frames         :initarg :frames         :accessor frames))) 
+;(defclass id3-tag ()
+;  ((identifier     :initarg :identifier     :accessor identifier)
+;   (major-version  :initarg :major-version  :accessor major-version)
+;   (revision       :initarg :revision       :accessor revision)
+;   (flag           :initarg :flag           :accessor flag)
+;   (size           :initarg :size           :accessor size)
+;   (frames         :initarg :frames         :accessor frames))) 
 
-
-(defun read-id3-tag (in)
-  (let ((tag (make-instance 'id3-tag)))
-    (with-slots (identifier major-version revision flags size frames) tag
-      (setf identifier     (read-iso-8859-1-string :length 3))
-      (setf major-version  (read-u1 in))
-      (setf revision       (read-u1 in))
-      (setf flags          (read-u1 in))
-      (setf size           (read-id3-encoded-size in ))
-      (setf frames         (read-id3-frames in :tag-size size)))
-    tag))
+;;;;code we are going to generate
+;(defun read-id3-tag (in)
+;  (let ((tag (make-instance 'id3-tag)))
+;    (with-slots (identifier major-version revision flags size frames) tag
+;      (setf identifier     (read-iso-8859-1-string :length 3))
+;      (setf major-version  (read-u1 in))
+;      (setf revision       (read-u1 in))
+;      (setf flags          (read-u1 in))
+;      (setf size           (read-id3-encoded-size in ))
+;      (setf frames         (read-id3-frames in :tag-size size)))
+;    tag))
 
 (defun as-keyword (sym) (intern (string sym) :keyword))
 
@@ -54,9 +54,9 @@
   (let ((name (first spec)))
     `(,name :initarg ,(as-keyword name) :accessor ,name)))
 
-(defmacro define-binary-class (name slots)
-  `(defclass ,name ()
-     ,(mapcar #'slot->defclass-slot slots)))
+;(defmacro define-binary-class (name slots)
+;  `(defclass ,name ()
+;     ,(mapcar #'slot->defclass-slot slots)))
 
 (defgeneric read-value (type stream &key)
   (:documentation "read a value of the type given from the stream."))
@@ -71,15 +71,26 @@
 (defun mklist (x)
   (if (listp x) x (list x)))
 
-(defmacro define-binary-class (name slots)
+(defmacro define-binary-class (name (&rest superclasses) slots)
   (with-gensyms (typevar objectvar streamvar)
     `(progn
-       (defclass ,name ()
+       (defclass ,name ,superclasses
          ,(mapcar #'slot->defclass-slot slots))
        (defmethod read-value ((,typevar (eql ',name )) ,streamvar &key)
          (let ((,objectvar (make-instance ',name)))
            (with-slots ,(mapcar #'first slots) ,objectvar
              ,@(mapcar #'(lambda (x) (slot->read-value x streamvar)) slots))
-           ,objectvar)))))
+           ,objectvar))
+       (defmethod write-value ((,typevar (eql ',name)) ,streamvar &key)
+         (with-slots ,(mapcar #'first slots) ,objectvar
+           ,@(mapcar #'(lambda (x) (slot->write-value x streamvar )) slots))))))
 
+;;;writing
+
+(defgeneric write-value (type stream value &key)
+  (:documentation "writes a value of the given type to stream."))
+
+(defun slot->write-value (spec stream)
+  (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+  `(write-value ',type ,stream ,name ,@args)))
 
